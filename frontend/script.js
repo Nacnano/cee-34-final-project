9e5685681dcb2203bd417c26626267a84e7d7b56
Maekin <74455990+mekintown@users.noreply.github.com>
2023-04-28 17:34:16 +0700

    Add GotoBtn and ToDayBtn handler

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,11 @@
-const calendar = document.querySelector(".calendar"),
-  date = document.querySelector(".date");
-daysList = document.querySelector(".days");
-prevBtn = document.querySelector(".prev-month-btn");
-nextBtn = document.querySelector(".next-month-btn");
+const calendar = document.querySelector(".calendar");
+const date = document.querySelector(".date");
+const daysList = document.querySelector(".days");
+const prevBtn = document.querySelector(".prev-month-btn");
+const nextBtn = document.querySelector(".next-month-btn");
+const todayBtn = document.querySelector(".today-btn");
+const gotoBtn = document.querySelector(".goto-btn");
+const dateInput = document.querySelector(".date-input");
 
 let currDay = new Date();
 let activeDay;
@@ -72,4 +75,63 @@ function createCalendar() {
   daysList.innerHTML = days;
 }
 
+function prevMonthBtnHandler() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  createCalendar();
+}
+
+prevBtn.addEventListener("click", prevMonthBtnHandler);
+
+function nextMonthBtnHandler() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  createCalendar();
+}
+
+nextBtn.addEventListener("click", nextMonthBtnHandler);
 createCalendar();
+
+todayBtn.addEventListener("click", () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  createCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+  if (dateInput.value.length === 2) {
+    dateInput.value += "/";
+  }
+  if (dateInput.value.length > 7) {
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+  if (e.inputType === "deleteContentBackward") {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      dateInput.value = "";
+      createCalendar();
+      return;
+    }
+  }
+  alert("Invalid Date");
+}
