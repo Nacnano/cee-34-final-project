de3d54faf7a6e9195f5957ac0e47044a214a9b01
Maekin <74455990+mekintown@users.noreply.github.com>
2023-04-28 16:52:40 +0700

    Add javascript to add date according to month

---
+++ frontend/script.js
@@ -0,0 +1,75 @@
+const calendar = document.querySelector(".calendar"),
+  date = document.querySelector(".date");
+daysList = document.querySelector(".days");
+prevBtn = document.querySelector(".prev-month-btn");
+nextBtn = document.querySelector(".next-month-btn");
+
+let currDay = new Date();
+let activeDay;
+let month = currDay.getMonth();
+let year = currDay.getFullYear();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function createCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = months[month] + " " + year;
+
+  let days = "";
+
+  for (let x = day; x > 0; x--) {
+    days += `<div class="day prev-month">${prevDays - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDate; i++) {
+    let event = false;
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      activeDay = i;
+      //getActiveDay(i);
+      //updateEvents(i);
+      if (event) {
+        days += `<div class="day today active event">${i}</div>`;
+      } else {
+        days += `<div class="day today active">${i}</div>`;
+      }
+    } else {
+      if (event) {
+        days += `<div class="day event">${i}</div>`;
+      } else {
+        days += `<div class="day ">${i}</div>`;
+      }
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next-month">${j}</div>`;
+  }
+
+  daysList.innerHTML = days;
+}
+
+createCalendar();
