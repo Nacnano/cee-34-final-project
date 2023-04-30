0e63977e5ae7eef718563688df945a0045ddcb83
nac <chotpisit080916c@gmail.com>
2023-04-30 23:28:50 +0700

    change to type module

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const dotenv = require('dotenv')
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import session from 'express-session'
+import dotenv from 'dotenv'
 dotenv.config({ path: './config.env' })
 
-const AppError = require('./utils/appError')
-const itemsRoutes = require('./routes/itemRoutes')
-const coursevilleRoutes = require('./routes/coursevilleRoutes')
+import AppError from './utils/appError.js'
+import coursevilleRoutes from './routes/coursevilleRoutes.js'
+import * as coursevilleController from './controller/coursevilleController.js'
 
 const app = express()
 
@@ -32,7 +32,6 @@ app.use(session(sessionOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/items', itemsRoutes)
 app.use('/courseville', coursevilleRoutes)
 app.get('/', (req, res) => {
   res.send('Congratulation. This server is successfully run.')
@@ -42,4 +41,4 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 })
 
-module.exports = app
+export default app
--- backend/routes/coursevilleRoutes.js
+++ backend/routes/coursevilleRoutes.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const coursevilleController = require('../controller/coursevilleController')
+import { Router } from 'express'
+import * as coursevilleController from '../controller/coursevilleController.js'
+import auth from '../middleware/auth.js'
 
-const router = express.Router()
+const router = Router()
 
 router.get('/auth_app', coursevilleController.authApp)
 router.get('/access_token', coursevilleController.accessToken)
-router.get('/get_profile_info', coursevilleController.getProfileInformation)
-router.get('/get_courses', coursevilleController.getCourses)
-router.get(
-  '/get_course_assignments/:cv_cid',
-  coursevilleController.getCoursesAssignments
-)
+router.get('/me', auth, coursevilleController.getProfileInformation)
+router.get('/get_courses', auth, coursevilleController.getCourses)
 router.get('/logout', coursevilleController.logout)
 
-module.exports = router
+export default router
